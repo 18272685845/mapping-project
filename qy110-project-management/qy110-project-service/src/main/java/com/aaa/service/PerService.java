84d6a64a5b318999b9000ca4ca29a686056516ed
eyew <1969926077@qq.com>
2020-07-16 21:41:05 +0800

    Update PerService.java

--- qy110-project-management/qy110-project-service/src/main/java/com/aaa/service/PerService.java
+++ qy110-project-management/qy110-project-service/src/main/java/com/aaa/service/PerService.java
@@ -1,6 +1,7 @@
 package com.aaa.service;
 
 import com.aaa.mapper.MenuMapper;
+import com.aaa.mapper.PerMapper;
 import com.aaa.model.Menu;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,19 +15,19 @@ import java.util.List;
 @Service
 public class PerService{
     @Autowired
-    private MenuMapper menuMapper;
+    private PerMapper perMapper;
 
     public List<Menu> selectAllMenu(Long userid) {
         //1.一级菜单
-        List<Menu> oneMenu = menuMapper.selectAllMenu((long) 0,userid);
+        List<Menu> oneMenu = perMapper.selectAllMenu((long) 0,userid);
         if (oneMenu.size()>0 && oneMenu != null){
             for (Menu menusList:oneMenu) {
 
                 //2.二级菜单
-                List<Menu> twoMenu=menuMapper.selectAllMenu(menusList.getMENU_ID(),userid);
+                List<Menu> twoMenu=perMapper.selectAllMenu(menusList.getMENU_ID(),userid);
                 for (Menu menusList2:twoMenu) {
                     //3.三级菜单
-                    List<Menu> threeMenu=menuMapper.selectAllMenu(menusList2.getMENU_ID(), userid);
+                    List<Menu> threeMenu=perMapper.selectAllMenu(menusList2.getMENU_ID(), userid);
                     for (Menu menuList3:threeMenu) {
                         menusList2.setListMenu(threeMenu);
                     }
