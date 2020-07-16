42649dd679514e43a0f8ecc2f0df32e92f37808d
eyew <1969926077@qq.com>
2020-07-16 14:44:38 +0800

    Update SpringCloudService.java

--- qy110-project-management/qy110-project-api/src/main/java/com/aaa/service/SpringCloudService.java
+++ qy110-project-management/qy110-project-api/src/main/java/com/aaa/service/SpringCloudService.java
@@ -9,10 +9,7 @@ import com.aaa.vo.DeptVo;
 import com.aaa.vo.DictVo;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -130,4 +127,12 @@ public interface SpringCloudService {
      */
     @GetMapping("/selectAllUser")
     public ResultData selectAllUser();
+
+    /**
+     *      查询菜单
+     * @param userid
+     * @return
+     */
+    @GetMapping("/selectAllMenu")
+    ResultData selectAllMenu(@RequestParam("userid") Long userid);
 }
