5d2db7e176bae166f63104ef2d92c0916b84e1f3
eyew <1969926077@qq.com>
2020-07-16 15:12:37 +0800

    Create PerService.java

---
+++ qy110-project-management/qy110-project-service/src/main/java/com/aaa/service/PerService.java
@@ -0,0 +1,41 @@
+package com.aaa.service;
+
+import com.aaa.mapper.MenuMapper;
+import com.aaa.model.Menu;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+/**
+ * @author 王登辉
+ * @date 2020/7/16
+ *      权限
+**/
+@Service
+public class PerService{
+    @Autowired
+    private MenuMapper menuMapper;
+
+    public List<Menu> selectAllMenu(Long userid) {
+        //1.一级菜单
+        List<Menu> oneMenu = menuMapper.selectAllMenu((long) 0,userid);
+        if (oneMenu.size()>0 && oneMenu != null){
+            for (Menu menusList:oneMenu) {
+
+                //2.二级菜单
+                List<Menu> twoMenu=menuMapper.selectAllMenu(menusList.getMENU_ID(),userid);
+                for (Menu menusList2:twoMenu) {
+                    //3.三级菜单
+                    List<Menu> threeMenu=menuMapper.selectAllMenu(menusList2.getMENU_ID(), userid);
+                    for (Menu menuList3:threeMenu) {
+                        menusList2.setListMenu(threeMenu);
+                    }
+                }
+                menusList.setListMenu(twoMenu);
+            }
+            return oneMenu;
+        }else {
+            return null;
+        }
+    }
+}
